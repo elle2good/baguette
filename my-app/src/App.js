088670c66b4adeee17695c01ecf511d1416c2518
kwakkyeongrok <kyeongrok@naver.com>
2023-05-19 22:47:38 +0900

    v2.11.0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -38,6 +38,11 @@ import ERC721 from "./abi/ERC721.json";
 import ERC1155 from "./abi/ERC1155.json";
 import Reward from "./abi/Reward.json";
 
+////////////////
+// API import //
+////////////////
+import { Wrapper } from "@googlemaps/react-wrapper";
+
 /////////////////
 // Page import //
 /////////////////
@@ -45,6 +50,7 @@ import LandingPage from "./pages/LandingPage";
 import AboutPage from "./pages/AboutPage";
 import BreadcrumbTrailPage from "./pages/BreadcrumbTrailPage";
 import GatheringsPage from "./pages/GatheringsPage";
+import GatheringProfilePage from "./pages/GatheringProfilePage";
 import ProfilePage from "./pages/ProfilePage";
 import SetProfilePage from "./pages/SetProfilePage";
 // 삭제 예정
@@ -62,10 +68,16 @@ import NFTTeamMintPage from "./pages/admin/NFTTeamMintPage";
 //////////////////////
 import Nav from "./components/Nav";
 
+
 /////////
 // CSS //
 /////////
 import "./App.css";
+
+///////////////////
+// place library //
+/////////////////// 
+const placesLibrary = ['places']
 
 ///////////////
 // App Class //
@@ -118,6 +130,14 @@ class App extends Component {
     this.setupAccountsChangedEventListener();
     this.setupNetworkChangedEventListener();
     this.fetchImageMetadata();
+
+    // 구글맵 로드 (api키), 라이브러리
+    this.loadScript();
+    // 현재위치 찍기
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      this.setState({ currentLocation: { lat: latitude, lng: longitude } });
+    });
   }
 
   //handlePageChange는 VotePage.js에서 투표 페이지를 보여줌
@@ -759,138 +779,171 @@ class App extends Component {
     return data;
   };
 
+  ///////////////
+  //////지도//////
+  ///////////////
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      currentLocation: null,
+      isLoaded: false,
+    };
+  }
+
+  loadScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_API}&libraries=${placesLibrary.join(',')}`;
+    script.async = true;
+    script.onload = () => {
+      this.setState({ isLoaded: true });
+    };
+    document.body.appendChild(script);
+  }
+
+  handleChange(e) {
+    this.setState({ id: e.target.value });
+  }
+
+
+
   render() {
+    const { isLoaded, currentLocation } = this.state;
     return (
-      <div>
-        <Nav />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <LandingPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fetchImageMetadata={this.fetchImageMetadata}
-                fTokenBalanceOf={this.fTokenBalanceOf}
-                addTokenToMetaMask={this.addTokenToMetaMask}
-                fNickname={this.fNickname}
+        <div>
+          <Nav />
+          <Wrapper apiKey={process.env.REACT_APP_MAP_API}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <LandingPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fetchImageMetadata={this.fetchImageMetadata}
+                    fTokenBalanceOf={this.fTokenBalanceOf}
+                    addTokenToMetaMask={this.addTokenToMetaMask}
+                    fNickname={this.fNickname}
+                  />
+                }
               />
-            }
-          />
-          {/* Menu */}
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/breadcrumb" element={<BreadcrumbTrailPage />} />
-          <Route path="/gatherings" element={<GatheringsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+              {/* Menu */}
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/breadcrumb" element={<BreadcrumbTrailPage />} />
+              <Route path="/gatherings" element={<GatheringsPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
 
-          {/* Set Profile */}
-          <Route
-            path="/set_profile"
-            element={
-              <SetProfilePage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fetchImageMetadata={this.fetchImageMetadata}
-                fMintByETH={this.fMintByETH}
+              {/* Set Profile */}
+              <Route
+                path="/set_profile"
+                element={
+                  <SetProfilePage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fetchImageMetadata={this.fetchImageMetadata}
+                    fMintByETH={this.fMintByETH}
+                  />
+                }
               />
-            }
-          />
+              {/* 지도 */}
+              <Route path='/gatherings' element={isLoaded && <GatheringsPage currentLocation={currentLocation} />} />
 
-          {/* 임시 페이지 (삭제 예정) */}
-          <Route
-            path="/eth_transfer"
-            element={
-              <ETHTransferPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                transferETH={this.transferETH}
+              <Route path='/gatherings/:id' element={<GatheringProfilePage />} />
+              {/* 임시 페이지 (삭제 예정) */}
+              <Route
+                path="/eth_transfer"
+                element={
+                  <ETHTransferPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    transferETH={this.transferETH}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/token_transfer"
-            element={
-              <TokenTransferPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                transferToken={this.transferToken}
+              <Route
+                path="/token_transfer"
+                element={
+                  <TokenTransferPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    transferToken={this.transferToken}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/nft_mint"
-            element={
-              <NFTMintPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fMintByETH={this.fMintByETH}
+              <Route
+                path="/nft_mint"
+                element={
+                  <NFTMintPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fMintByETH={this.fMintByETH}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/ft_mint"
-            element={
-              <FTMintPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fMintByETH_FT={this.fMintByETH_FT}
-                fBalanceOf={this.fBalanceOf}
-                fCheckAndClaimGoodToken={this.fCheckAndClaimGoodToken}
+              <Route
+                path="/ft_mint"
+                element={
+                  <FTMintPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fMintByETH_FT={this.fMintByETH_FT}
+                    fBalanceOf={this.fBalanceOf}
+                    fCheckAndClaimGoodToken={this.fCheckAndClaimGoodToken}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/exchange_token"
-            element={
-              <ExchangeTokenPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fExchangeEther={this.fExchangeEther}
+              <Route
+                path="/exchange_token"
+                element={
+                  <ExchangeTokenPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fExchangeEther={this.fExchangeEther}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/vote"
-            element={
-              <VotePage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fStartVote={this.fStartVote}
-                fVote={this.fVote}
-                fVotes_view={this.fVotes_view}
-                voteData={this.state.voteData}
-                currentPage={this.state.currentPage}
-                onPageChange={this.handlePageChange} // Pass handlePageChange to VotePage
-                fHasVoted={this.fHasVoted}
-                fSetApprovalForAll={this.fSetApprovalForAll}
+              <Route
+                path="/vote"
+                element={
+                  <VotePage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fStartVote={this.fStartVote}
+                    fVote={this.fVote}
+                    fVotes_view={this.fVotes_view}
+                    voteData={this.state.voteData}
+                    currentPage={this.state.currentPage}
+                    onPageChange={this.handlePageChange} // Pass handlePageChange to VotePage
+                    fHasVoted={this.fHasVoted}
+                    fSetApprovalForAll={this.fSetApprovalForAll}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/admin/controller"
-            element={
-              <ControllerPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fAddController={this.fAddController}
-                fRemoveController={this.fRemoveController}
-                fSetSale={this.fSetSale}
+              <Route
+                path="/admin/controller"
+                element={
+                  <ControllerPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fAddController={this.fAddController}
+                    fRemoveController={this.fRemoveController}
+                    fSetSale={this.fSetSale}
+                  />
+                }
               />
-            }
-          />
-          <Route
-            path="/admin/team_mint"
-            element={
-              <NFTTeamMintPage
-                state={this.state}
-                connectToMetaMask={this.connectToMetaMask}
-                fTeamMint={this.fTeamMint}
+              <Route
+                path="/admin/team_mint"
+                element={
+                  <NFTTeamMintPage
+                    state={this.state}
+                    connectToMetaMask={this.connectToMetaMask}
+                    fTeamMint={this.fTeamMint}
+                  />
+                }
               />
-            }
-          />
-        </Routes>
-      </div>
-    );
+            </Routes>
+          </Wrapper>
+        </div>)
   }
 }
 
---
+++ my-app/src/components/GatheringPreview.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const GatheringPreview = ({ gatherList }) => {
+    const navigate = useNavigate();
+    const goToGathering = () => {
+        navigate(`/map/${gatherList.id}`)
+        console.log(gatherList);
+    }
+
+    return (
+        <div>
+            <div>{gatherList.title}</div>
+            <div>{gatherList.sDate} ~ {gatherList.eDate}</div>
+            <div><button onClick={goToGathering}>Details</button></div>
+        </div>
+    )
+}
+
+export default GatheringPreview
